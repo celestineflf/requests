efd54053fd48236ac98aff3be97a2653e6f2906e
celestineflf <154523793+celestineflf@users.noreply.github.com>
2025-04-01 14:29:12 +0000

    repurpose

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import ticket
-from . import hub
-from . import support
-from . import logistic
-from . import sales
-from . import purchase
-from . import finance
-from . import management
-
